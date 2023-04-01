9c2f443131c6175f3dc09e985bc6ab4a10726656
astsbins <astsbins@gmail.com>
2023-04-01 23:34:55 +1100

    hello commit

---
+++ src/main/java/Hello_Maven.java
@@ -0,0 +1,8 @@
+public class Hello_Maven {
+    public static void main(String[] args) {
+        System.out.println(hello());
+    }
+    public static String hello(){
+        return("Hello Maven");
+    }
+}
---
+++ src/test/java/Hello_MavenTest.java
@@ -0,0 +1,9 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+class Hello_MavenTest {
+
+    @org.junit.jupiter.api.Test
+    void testHello() {
+        assertEquals("Hello Maven",Hello_Maven.hello());
+    }
+}
